5fe60f8baaea8fb63472b62e7df9ab27e88b835e
MSI\Anoeska Clement <a.c.lement6@gmail.com>
2019-02-08 14:31:10 +0100

    Background and picturse found.

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Stemwijzer.</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 
+	<div class="block">
+		<button type="submit" id="start">Start</button>
+	</div>
+
+
+<script type="text/javascript"> src="script.js"</script>
+<script type="text/javascript"> src="object.js"</script>
 </body>
 </html>
