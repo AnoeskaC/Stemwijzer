a1a73e81f9431328203d6065844b2c0c9a3325ca
MSI\Anoeska Clement <a.c.lement6@gmail.com>
2019-02-13 14:32:52 +0100

    added css and javascript

--- index.php
+++ index.php
@@ -8,11 +8,25 @@
 <body>
 
 	<div class="block">
-		<button type="submit" id="start">Start</button>
+
+		<img src="foto/logostemwijzer.png" id="stemwijzer">
+		
+		<h2 id="h2">Test uw politieke voorkeur aan de hand van 30 stellingen</h2>
+
+		<button id="agreeButton" onclick="agree()"></button>
+		<button id="neitherButton" onclick="neither()"></button>
+		<button id="disagreeButton" onclick="disagree()"></button>
+		<button id="startButton" onclick="start()">Start</button>
+
+		<span id="partijen"><p>Wat vinden de partijen?</p></span>
+
+		<p>Wat hebben de partijen in het verleden waargemaakt van hun verkiezingsbeloften? 'Track' uw stem met de Stemmentracker, een website die terugblikt op 30 Tweede Kamerstemmingen van de afgelopen regeerperiode. Met welke partij stemde u mee?</p>
+
 	</div>
 
 
+
 <script type="text/javascript"> src="script.js"</script>
-<script type="text/javascript"> src="object.js"</script>
+<script type="text/javascript"> src="data.js"</script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function{
-
+function agree(){
+	document.getElementById("firstChoice").innerHTML = "Eens";
 };
 
+function neither() {
+	
+}
+
+function disagree(){
+
+}
+
+function start(){
+	document.getElementById("agreeButton").innerHTML = "Eens";
+	document.getElementById("agreeButton").style.display="inline";
+	document.getElementById("neitherButton").innerHTML = "Geen van beide";
+	document.getElementById("neitherButton").style.display="inline";
+	document.getElementById("disagreeButton").innerHTML = "Oneens";
+	document.getElementById("disagreeButton").style.display="inline";
+	document.getElementById("startButton").style.display="none";
+}
